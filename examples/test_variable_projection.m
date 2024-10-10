69b01a205f56633ecf04ad1602ea8db2b421f3c7
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-10-10 12:10:20 +0100

    add a third sinusoid and plot DMD modes

--- examples/test_variable_projection.m
+++ examples/test_variable_projection.m
@@ -20,7 +20,7 @@ contourf(signal_generator.T', ...
     signal_generator.X', ...
     signal_generator.components{1}.signal',...
     LineStyle="none")
-title("Sinusoid 1")
+title(["Sinusoid 1, Omega=" num2str(round(omega, 3)) ", Amp=" num2str(round(a, 3))])
 
 % second sinusoid
 
@@ -37,12 +37,30 @@ contourf(signal_generator.T', ...
     signal_generator.X', ...
     signal_generator.components{2}.signal',...
     LineStyle="none")
-title("Sinusoid 2")
+title(["Sinusoid 2, Omega=" num2str(round(omega, 3)) ", Amp=" num2str(round(a, 3))])
+
+% third sinusoid
+
+a=3;
+c=-1.5;
+k=0.5;
+omega=5;
+
+signal_generator = add_sinusoid2( ...
+    signal_generator, a, k, omega, c);
+
+figure
+contourf(signal_generator.T', ...
+    signal_generator.X', ...
+    signal_generator.components{3}.signal',...
+    LineStyle="none")
+title(["Sinusoid 3, Omega=" num2str(round(omega, 3)) ", Amp=" num2str(round(a, 3))])
+
 
 % add noise and plot the final result
 
 signal_generator = add_noise( ...
-    signal_generator, 0.1, 42);
+    signal_generator, 0.2, 42);
 
 figure
 contourf(signal_generator.T', ...
@@ -71,11 +89,25 @@ end
 
 %% perform opt DMD using Algorithm 3
 
-r = 4;
+r = 6;
 imode = 2;
 [w, e2, b] = optdmd( ...
     X_delay, ...
     t_delay, ...
     r, ...
     imode);
+
+disp("Eigenvalues:")
+disp(e2')
+disp("Amplitudes:")
+disp(b')
+
+%% plot the DMD modes
+
+for i = 1:r/2
+    j = i*2 - 1;
+    figure()
+    plot(signal_generator.x, real(w(1:100, j)))
+    title(["Omega=" num2str(round(imag(e2(j)), 3)) ", Amp=" num2str(round(b(j), 3))])
+end
 
